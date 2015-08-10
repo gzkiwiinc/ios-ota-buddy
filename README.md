f390365524bbe6deac7dfc2d790931a362abdedf
CashLee <cashlee96347@gmail.com>
2015-08-10 15:15:57 +0800

    [mod]编写README以及review otabuddy的改进代码

--- README.md
+++ README.md
@@ -1,32 +1,35 @@
 iOS OTA Buddy
 =============
 
-Command line tool to aid in distributing iOS appliactions OTA using the ITMS-protocol (supported by all iOS4 devices and later).
+### 内部OTA使用文档
 
-#### Overview
-The easiest way to distribute ad-hoc builds is to download them through Safari, this is supported from iOS4. OTA-builds are perfect when you want to distribute to a limited audience, e.g. nightly builds, test builds, debug builds etc.
-After building your application archive (.ipa) and signing it with your ad-hoc distribution provisioning profile you some additional steps to enable OTA distribution.
+#### 局域网共享服务器设置
 
-First of all, you need a web-server or dropbox account to put the files. If you want to use Dropbox, simply put your files in the Public folder and right-click to get the public URL.
+  * 将测试ipa文件放到“共享服务器->公共文件夹->distribution文件夹”内
+  
+  * 双击运行“start.command”文件
+  
+  * 访问“192.168.1.239:8080”，点击“Install App”即可
+  
+#### Linode设置
 
-To download the application from your phone you need an _itms-services_ link that points to a _plist_-file containing some metadata regarding your application. The plist contains (among other things) the URL to the actual application file (.ipa).
+  * 将测试ipa文件放到东京linode主机的指定文件夹内
+  
+  * 运行"otabuddy.sh"文件
+  
+  * 访问"[域名]/distribution/index.html"，用户只需点击“Install App”即可
 
-To install an ad-hoc application on your device you need to install the provisioning profile used, which also needs to contain your device UUID (see Apple's documentation on this).
 
-This tool will help you extract the provisioning profile used, create the necessary .plist and generate the itms-link.
+#### 七牛命令行工具配置
 
-#### Step by step
+  * http://developer.qiniu.com/docs/v6/tools/qrsync.html
 
-##### Setup
-1. Build your application archive with Xcode or xcodebuild
-2. In Xcode Organizer, select Share, sign your application and save the application.ipa
-3. Move the application.ipa and otabuddy.sh file to your distribution folder (on the web-server/dropbox)
-4. Enable execution "chmod +x otabuddy.sh"
+  需要配置七牛命令行
 
-#### Usage
-5. Extract the .mobileprovision to adhoc.mobileprovision by running "./otabuddy provisioning application.ipa adhoc.mobileprovision"
-6. Create the .plist file by running "./otabuddy.sh plist application.ipa http://domain.com/path/distribution/application.ipa application.plist"
-7. Generate the itms-services link by running "./otabuddy.sh itms http://domain.com/path/distribution/application.plist"
-8. Create a HTML-file with an anchor with the itms-link created in step 7 and a link to the provisioning profile extracted in 5.
-9. Visit your HTML-file and download your application. Remember to download the provisioning profile first.
-10. Enjoy!
+#### OTA文件改进事项
+
+  1，配置上更人性化，在otabuddy.sh 文件上
+
+  2，能够连接更多自动化脚本，提供一定的接口
+
+  3，能够建立一个自动化运作的小系统
--- otabuddy.sh
+++ otabuddy.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 #
 # Copyright Sveinung Kval Bakken 2012
@@ -6,108 +7,137 @@
 # Please keep this comment, but copy and modify anything below as you want.
 # source: https://github.com/sveinungkb/ios-ota-buddy
 
-PLIST_BUDDY="/usr/libexec/PlistBuddy -c"
-
 provisioning() 
 {
-	if [ -z "$2" ]; then
-		unzip -p "$1" "**.mobileprovision"
-	else
-		unzip -p "$1" "**.mobileprovision" > $2
-		echo $"Extracted the .mobileprovison in $1 to: $2"
-	fi
+  if [ -z "$2" ]; then
+    unzip -p "$1" "**.mobileprovision"
+  else
+    unzip -p "$1" "**.mobileprovision" > $2
+    echo $"Extracted the .mobileprovison in $1 to: $2"
+  fi
 }
 
 urlencode()
 {
-	echo $1 | python -c 'import sys,urllib;print urllib.quote_plus(sys.stdin.read().strip())'
+  echo $1 | python -c 'import sys,urllib;print urllib.quote_plus(sys.stdin.read().strip())'
 }
 
 otaplist()
 {
-	if [ -z "$2" ]; then
-		echo "Missing URL parameter"
-		printusage
-		exit 1
-	elif [ -z "$3" ]; then
-		echo "Missing output file parameter"
-		printusage
-		exit 1
-	else
-		# Extract IPA-files
-		APP_PLIST=temp.plist
-		OTA_PLIST=$3
-		unzip -p "$1" "**/Info.plist" > $APP_PLIST
+    cd "/home/deploy/distribution" 
+    file=`find ./plist . -name "*.plist" | xargs ls -lt | awk '{print $9}'`
+    for i in $file
+    do
+        filename=$i
+        break
+    done
+    filename=`basename $filename`
+    OLD_OTA_PLIST=$filename
+    rm -rf ./plist/*.plist
+         
+    file=`find -P . -name "*.ipa" | xargs ls -lt | awk '{print $9}'`
+    for i in $file
+    do
+        filename=$i
+        break
+    done
+    filename=`basename $filename`
 
-		#Read contents
-		BUNDLE_IDENTIFIER=$($PLIST_BUDDY "Print CFBundleIdentifier" $APP_PLIST)
-		BUNDLE_NAME=$($PLIST_BUDDY "Print CFBundleDisplayName" $APP_PLIST)
+    DATE=`date +"%Y%m%d-%H%M%S"`
+    OTA_PLIST=application-$DATE.plist
+    IPA_NAME=$filename
+    # URL="http://test.yongjia.fm/distribution/$IPA_NAME"
+  
+    # generate plist
+    generatePlist "$IPA_NAME" "$OTA_PLIST"
 
-		# Clean up
-		rm $APP_PLIST
-	
-		# Create .plist
-		$PLIST_BUDDY "Add :items array" $OTA_PLIST
-		$PLIST_BUDDY "Add :items:0:metadata dict" $OTA_PLIST
-		$PLIST_BUDDY "Add :items:0:metadata:bundle-identifier string $BUNDLE_IDENTIFIER" $OTA_PLIST
-		$PLIST_BUDDY "Add :items:0:metadata:title string $BUNDLE_NAME" $OTA_PLIST
-		$PLIST_BUDDY "Add :items:0:metadata:kind string software" $OTA_PLIST
-		$PLIST_BUDDY "Add :items:0:assets array" $OTA_PLIST
-		$PLIST_BUDDY "Add :items:0:assets:0 dict" $OTA_PLIST
-		$PLIST_BUDDY "Add :items:0:assets:0:kind string software-package" $OTA_PLIST
-		$PLIST_BUDDY "Add :items:0:assets:0:url string $2" $OTA_PLIST
-		
-		echo "Created $OTA_PLIST with values:"
-		echo "Bundle identifier: $BUNDLE_IDENTIFIER"
-		echo "Title:             $BUNDLE_NAME"
-		echo "URL to app:        $2"
-	fi
+    # sync
+    qsync
+
+    # refresh html
+    refreshHtml "$OLD_OTA_PLIST" "$OTA_PLIST"
 }
 
 itms()
 {
-	if [ -z $1 ]; then
-		echo "Missing URL to .plist"
-		printusage
-		exit 1
-	else
-		echo "It can be downloaded with this link:"
-		echo "itms-services://?action=download-manifest&url=$(urlencode $1)"
-		echo "Example HTML anchor:"
-		echo "<a href=\"itms-services://?action=download-manifest&url=$(urlencode $1)\">Download my application</a>"
-	fi
+  if [ -z $1 ]; then
+    echo "Missing URL to .plist"
+    printusage
+    exit 1
+  else
+    echo "It can be downloaded with this link:"
+    echo "itms-services://?action=download-manifest&url=$(urlencode $1)"
+    echo "Example HTML anchor:"
+    echo "<a href=\"itms-services://?action=download-manifest&url=$(urlencode $1)\">Download my application</a>"
+  fi
 }
 
 printusage()
 {
-	echo "Usage:"
-	echo $"$0 provisioning: Will extract the embedded provisioning profile from your application.ipa"
-	echo $" $0 provisioning file.ipa, will print the provisioning profile to STDOUT, pipe it where you want (can be used to download)"
-	echo $" $0 provisioning file.ipa name.mobileprovision, will extract the provisioning profile to name.mobileprovision"
-	echo ""
-	echo $"$0 plist: Will generate the .plist required for OTA-distribution"
-	echo $" $0 plist file.ipa http://domain.com/path/distribution/file.ipa application.plist"
-	echo ""
-	echo $"$0 itms: Will generate an itms-services link that can be used to download your application."
-	echo $" $0 itms http://domain.com/path/distribution/application.plist"
-	exit 1
+  echo "Usage:"
+  echo $"$0 provisioning: Will extract the embedded provisioning profile from your application.ipa"
+  echo $" $0 provisioning file.ipa, will print the provisioning profile to STDOUT, pipe it where you want (can be used to download)"
+  echo $" $0 provisioning file.ipa name.mobileprovision, will extract the provisioning profile to name.mobileprovision"
+  echo ""
+  echo $"$0 plist: Will generate the .plist required for OTA-distribution"
+  echo $" $0 plist file.ipa http://domain.com/path/distribution/file.ipa application.plist"
+  echo ""
+  echo $"$0 itms: Will generate an itms-services link that can be used to download your application."
+  echo $" $0 itms http://domain.com/path/distribution/application.plist"
+  exit 1
 }
 
-if [ -z "$2" ]; then
-printusage
-fi
+generatePlist()
+{
+   IPA_NAME=$1
+   OTA_PLIST=$2
+   text=`sed -e "s/ipa_path/http:\/\/test.yongjia.fm\/distribution\/$IPA_NAME/" template.plist`
+   echo $text > ./plist/$OTA_PLIST
+}
+
+getipa()
+{
+    file=`find -P . -name "*.ipa" | xargs ls -lt | awk '{print $9}'`
+    for i in $file
+    do
+        echo $i
+        break
+    done
+    #filename=`basename $file`
+    #return filename
+}
+
+refreshHtml()
+{
+    # OTA_PLIST=$0
+    cd "/home/deploy/distribution"
+    text=`sed -e "s/$1/$2/" index.html`
+    echo $text > index.html
+}
+
+qsync()
+{
+    cd "/home/deploy/distribution/qiniu-devtools"
+    `./qrsync config.json`
+}
+
+#if [ -z "$2" ]; then
+#printusage
+#fi
 
 case "$1" in
-		provisioning)
-			provisioning "$2" "$3"
-			;;
-		plist)
-			otaplist "$2" "$3" "$4"
-			;;
-		itms)
-			itms "$2"
-			;;
-		*)
-			printusage
-			
+    provisioning)
+      provisioning "$2" "$3"
+      ;;
+    plist)
+            echo "hello"
+      otaplist "$2" "$3" "$4"
+      ;;
+    itms)
+      itms "$2"
+      ;;
+    *)
+      # printusage
+      otaplist
+      
 esac
